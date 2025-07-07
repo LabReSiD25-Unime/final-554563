0624f8d4040e46b8b29eaf1bfa430f0eee7f71ba
Faby <54766182+faby1507@users.noreply.github.com>
2025-07-07 15:33:50 +0200

    Update database.h

--- database.h
+++ database.h
@@ -3,19 +3,17 @@
 
 #include <sqlite3.h>
 
-int open_db(sqlite3 **db);
+int open_db(sqlite3 **db); //serve per far sì che la funzione possa restituire un database aperto modificando il puntatore passato
 void close_db(sqlite3 *db);
 int init_db(sqlite3 *db);
 
 //serializza in JSON
 char *get_enti_json(sqlite3 *db);
 char *get_articoli_json(sqlite3 *db);   
-
-//blog
 char *get_blog_json(sqlite3 *db);
 
 //inserimenti
-int insert_ente(sqlite3 *db, const char *nome, const char *descr, const char *sede);  
+int insert_ente(sqlite3 *db, const char *nome, const char *descr, const char *sede); 
 int insert_donazione(sqlite3 *db, int id_utente, int id_ente, double importo);         
 int   insert_post(sqlite3 *db, const char *nome, const char *msg);
 
