3af00ac8ff21c7e77de9696c6f8e8f75c92cc3aa
Faby <54766182+faby1507@users.noreply.github.com>
2025-07-07 10:16:24 +0200

    Update database.h

--- database.h
+++ database.h
@@ -7,16 +7,16 @@ int open_db(sqlite3 **db);
 void close_db(sqlite3 *db);
 int init_db(sqlite3 *db);
 
-/* Serializza in JSON (malloc, free a carico del chiamante) */
+//serializza in JSON
 char *get_enti_json(sqlite3 *db);
-char *get_articoli_json(sqlite3 *db);   // <--- QUESTA RIGA SERVE
+char *get_articoli_json(sqlite3 *db);   
 
-/* Blog */
+//blog
 char *get_blog_json(sqlite3 *db);
 
-/* Inserimenti */
-int insert_ente(sqlite3 *db, const char *nome, const char *descr, const char *sede);   // <--- SERVE
-int insert_donazione(sqlite3 *db, int id_utente, int id_ente, double importo);         // <--- SERVE
+//inserimenti
+int insert_ente(sqlite3 *db, const char *nome, const char *descr, const char *sede);  
+int insert_donazione(sqlite3 *db, int id_utente, int id_ente, double importo);         
 int   insert_post(sqlite3 *db, const char *nome, const char *msg);
 
-#endif // SHARECARE_DATABASE_H
+#endif 
